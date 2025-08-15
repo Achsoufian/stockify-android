193bcabe4d4652e49c60dd7a8cb0dff6ee0e3400
Achsoufian <acherkisoufiane2000@gmail.com>
2025-08-15 21:39:41 +0100

    Update settings.gradle.kts

--- stockify-android-webview/settings.gradle.kts
+++ stockify-android-webview/settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "StockifyWebView"
+pluginManagement {
+    repositories {
+        google()            // Needed for com.android.application plugin
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "stockify-android-webview"
 include(":app")
